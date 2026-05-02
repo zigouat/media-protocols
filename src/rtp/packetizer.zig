4b23a07a3cdc0100059cacd217177151f7e86166
Billal GHILAS <bilal@evercam.io>
2026-05-02 16:10:38 +0100

    feat(rtp): add h264 packetizer

---
+++ src/rtp/packetizer.zig
@@ -0,0 +1,5 @@
+pub const H264 = @import("packetizer/h264.zig");
+
+test {
+    _ = @import("packetizer/h264.zig");
+}
--- src/rtp/rtp.zig
+++ src/rtp/rtp.zig
@@ -1,9 +1,11 @@
 pub const Packet = @import("packet.zig");
 pub const Depacketizer = @import("depacketizer.zig");
+pub const packetizer = @import("packetizer.zig");
 pub const H264Depacketizer = @import("depacketizer/h264.zig");
 
 test {
     _ = @import("packet.zig");
     _ = @import("depacketizer.zig");
+    _ = @import("packetizer.zig");
     _ = @import("depacketizer/h264.zig");
 }
